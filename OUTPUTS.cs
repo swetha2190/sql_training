a92c3c5fe3eb6023406db8ecf718e644ebb81ba1
swetha2190 <43073957+swetha2190@users.noreply.github.com>
2018-12-21 17:01:47 +0530

    Add files via upload

---
+++ OUTPUTS.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALL_BY_REF_AND_NUM
+{
+    class OUTPUTS
+    {
+
+        int a, b, c, d, e, f;
+
+        public void GetDetails()
+        {
+            Console.WriteLine("enter two numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void Calculate(int a, int b, out int c, out int d, out int f)
+        {
+            c = a + b;
+            d = a - b;
+            e = a * b;
+            f = a / b;
+        }
+
+        public void Display()
+        {
+            Calculate(a, b, out c, out d, out e, out f);
+            Console.WriteLine("add = {0}", c);
+            Console.WriteLine("subtract = {0}", d);
+            Console.WriteLine("multiply = {0}", e);
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            OUTPUTS o = new OUTPUTS();
+            o.GetDetails();
+            o.Calculate();
+            o.Display();
+
+
+        }
+    }
+            
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyProject
+
+//CALL BY VALUE CREATES MULTIPLE COPIES AND ALSO CHANGES R NOT REFLECTED BACK SO ITS NOT RECOMENDED...
+
+//CALL BY REF IS GOOD AND CHANGES R REFLECTED BACK.....SO NO WASTAGE OF MEMORY
+
+namespace CALL_BY_REF_AND_NUM
 {
     class Program
     {
+
+        int num;
+        public void GetDetails()
+        {
+            Console.WriteLine("enter a value");
+            num = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void Square(int num)
+        {
+            num = num * num;
+           
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("before square num={0}", num);
+
+             Square(num);
+            Console.WriteLine("After square num={0}", num);
+
+        }
+
         static void Main(string[] args)
         {
+
+            Program p = new Program();
+            p.GetDetails();
+            p.Display();
+            Console.ReadLine();
+
         }
     }
+
 }
